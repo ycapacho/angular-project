6fdd21af8106e2a0a792c7033375e90caa975ebe
Samuel Pérez <samusinho79@gmail.com>
2022-11-11 20:35:17 -0500

    configuraciones básicas. creación de componentes home y signin.  Enrutamiento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SigninComponent } from './components/signin/signin.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent
+}, {
+  path: 'signin',
+  component: SigninComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
