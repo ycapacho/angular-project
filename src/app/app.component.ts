46092125026ff2a03b52fa7df48154a2b9a9329f
Samuel Pérez <samusinho79@gmail.com>
2022-11-15 20:35:05 -0500

    loading instalado y configurado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './services/auth.service';
+import { NgxSpinnerService } from "ngx-spinner";
+
 
 @Component({
   selector: 'app-root',
@@ -8,10 +10,12 @@ import { AuthService } from './services/auth.service';
 })
 export class AppComponent implements OnInit {
   constructor(
-    private auth: AuthService
+    private auth: AuthService,
+    private spinner: NgxSpinnerService
   ) {}
 
   ngOnInit(): void {
-      this.auth.autoLogin();
+    this.spinner.show();
+    this.auth.autoLogin();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,6 +20,7 @@ import { NgxSpinnerModule } from "ngx-spinner";
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
